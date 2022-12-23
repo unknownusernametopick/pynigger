5da3cd1212474bee7fc43a6d937fa4d6f5eb8d93
M4hbod <88078156+unknownusernametopick@users.noreply.github.com>
2022-12-23 22:08:25 +0330

    Update constants.py

--- pynigger/constants.py
+++ pynigger/constants.py
@@ -1,4 +1,4 @@
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 __description__ = "An add-on extension to Pyrogram for absolute beginners"
 __copyright__ = "Copyright (C) 2021-2022 Nigger Bots <https://github.com/unknownusernametopick>"
 __license__ = "GNU General Public License v3 or later (GPLv3+)"
